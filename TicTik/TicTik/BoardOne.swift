b487664bf4b34e6ac8cc75d5e6d8bb60cc83cd9c
Oluwamayowa Olojo <45318880+omo7765@users.noreply.github.com>
2020-04-12 20:25:51 -0400

    Add files via upload

---
+++ TicTik/TicTik/BoardOne.swift
@@ -0,0 +1,30 @@
+//
+//  BoardOne.swift
+//  TicTik
+//
+//  Created by Mayo Olojo on 4/12/20.
+//  Copyright © 2020 Mayo Olojo. All rights reserved.
+//
+
+import UIKit
+
+class BoardOne: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TicTik/TicTik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTik
+//
+//  Created by Mayo Olojo on 4/11/20.
+//  Copyright © 2020 Mayo Olojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var boardOne : BoardOne?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func switchViews(_ sender: UIButton){
+        
+    }
+
+}
+
